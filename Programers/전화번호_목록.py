c7737b57127f1c681bb529e957c5a9aa38ed1ad2
ldonghae0230 <ldonghae320@gmail.com>
2024-01-03 17:46:12 +0900

    :memo: 문제 풀이: 해시 (LV2)

--- Programers/전화번호_목록.py
+++ Programers/전화번호_목록.py
@@ -1,45 +1,38 @@
+'''
+1. 유형: 해시
+2. 특징: LIKE 전화번호부. 정수가 아닌 값(예) string)을 키로 가질 수 있어 배열로 담을 수 없는 정보를 담을 수 있다.
+3. 문제: 프로그래머스 전화번호 목록
+4. 배운점: 2-1 단계에서 for n in num, temp+=n과 같이 작성해 temp를 key와 비교할 수도 있다.
+'''
 def solution(phone_book):
-    phone_book.sort()
-    n = len(phone_book)
+    answer = True
     
-    for idx in range(n-1):
-        l = len(phone_book[idx])
-        if l < len(phone_book[idx+1]):
-            if phone_book[idx] == phone_book[idx+1][:l]:
-                return False
-    return True
+    # 1) Dictionary Comprehesion으로 딕셔너리 생성
+    dict = {num : 0 for num in phone_book}
+    # print(dict)
+    # {'119': 0, '97674223': 0, '1195524421': 0}
+    
+    # 2) 전체 전화번호부에서 반복
+    # 2-1) 번호의 길이를 1에서 번호-1까지 자르고
+    # 2-2) 값이 딕셔너리에 있으면 False 반환
+    for num in phone_book: 
+        for i in range(1, len(num)):
+            if num[:i] in dict:
+                answer = False
+                return answer
+    return answer
 
 '''
-해시를 사용한 방법
-
+1차 시도: 효율성 테스트 시간 초과로 실패
 def solution(phone_book):
-    hash_map = {}
-    
-    for phone in phone_book:
-        hash_map[phone] = 1
-        
-    for phone in phone_book:
-        temp = ''
-        for num in phone:
-            temp += num
-            if (temp in hash_map) and (temp != phone):
-                return False
-    return True
-'''
-
-'''
-for문 2개 사용 시 시간초과 > sort() 함수 사용 시 길이와 값 둘 다 정렬되는 것 사용
-
-def solution(phone_book):
-    phone_book.sort()
-    n = len(phone_book)
-    
-    for i in range(n):
-        for j in range(n):
-            if i == j:
+    answer = True
+    for num1 in phone_book:
+        for num2 in phone_book:
+            if num1 == num2:
                 continue
-            length = len(phone_book[i])
-            if phone_book[j][:length] == phone_book[i]:
-                return False
-    return True
+            else:
+                if len(num1) < len(num2) and num2[:len(num1)] == num1:
+                    answer = False
+                    return answer
+    return answer
 '''
